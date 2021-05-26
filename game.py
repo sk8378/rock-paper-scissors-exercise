b956ca995911cbdf9beaad7faac6f244ae831159
sk8378 <84359100+sk8378@users.noreply.github.com>
2021-05-25 21:22:33 -0400

    finished game - pre username

--- game.py
+++ game.py
@@ -2,7 +2,13 @@
 
 import random
 
+print("--------------------------------------------------")
+
+print("Let's Play a Game")
+
 print("Rock, Paper, Scissors, Shoot!")
+
+print("--------------------------------------------------")
 
 user_choice = input("Choose an action 'rock','paper','scissors':")
 
@@ -15,7 +21,8 @@ print("You Chose: ", user_choice)
 #... and we'll ask the user to run the program again
 
 if (user_choice == "rock") or (user_choice == "paper") or (user_choice == "scissors"):
-    print("VALID.KEEP GOING")
+    print("--------------------------------------------------")
+
 else:
     print("OOPS, invalid input.  Please try again.")
     exit()
@@ -25,4 +32,22 @@ computer_choice = random.choice(valid_options)
 
 print("Computer Chose: ",computer_choice)
 
-print("THIS IS THE END OF OUR GAME.  PLEASE PLAY AGAIN.")
+print("--------------------------------------------------")
+
+print("Game Result:")
+
+if (user_choice == computer_choice):
+    print("Tie!")
+elif ((user_choice == "rock") and (computer_choice == "scissors")):
+    print("You win!")
+elif ((user_choice == "paper") and (computer_choice == "rock")):
+    print("You win!")
+elif ((user_choice == "scissors") and (computer_choice == "paper")):
+    print("You Win!")
+elif ((computer_choice == "rock") and (user_choice == "scissors")):
+    print("Computer Wins. Try again!")
+elif ((computer_choice == "paper") and (user_choice == "rock")):
+    print("Computer Wins. Try again!")
+elif ((computer_choice == "scissors") and (user_choice == "paper")):
+    print("Computer Wins. Try again!")
+    exit()
